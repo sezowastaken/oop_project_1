6cfe66c0c84aa41805b192e37e8bdc317fc5c809
sezowastaken <sezowastaken@stu.khas.edu.tr>
2025-10-27 00:54:42 +0300

    sezo push #3
    prime operations are all done
    but there can be improvements on getting the input "long" instead of "int"

--- secondary.java
+++ secondary.java
@@ -1,4 +1,5 @@
 import java.lang.classfile.CodeBuilder.CatchBuilder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -34,6 +35,8 @@ public class secondary {
                         eratosthenes(n);
                         System.out.println("Sieve of Sundaram Algorithm");
                         sundaram(n);
+                        System.out.println("Sieve of Atkin Algorithm");
+                        atkin(n);
                         break;
                     case "2", "Operation 2", "Evoluation of Expression":
                         clearConsole();
@@ -71,7 +74,6 @@ public class secondary {
     public static void eratosthenes(int n) {
         boolean[] prime = new boolean[n+1];
         int primesInRange = 0;
-        long startTime = System.nanoTime();
         for (int i = 0; i <= n; i++) {
             if (i == 0 || i == 1) {
                 prime[i] = false;
@@ -79,43 +81,41 @@ public class secondary {
             }
             prime[i] = true;
         }
+
+        long startTime = System.nanoTime();
         for (int p = 2; p*p <= n; p++) {
             if (prime[p]) {
                 for (int i = p*p; i <= n; i += p) {
                     prime[i] = false;
-                    primesInRange++;
                 }
             }
         }
         long endTime = System.nanoTime();
-        primesInRange = n - primesInRange;
-        int primesInRangeTemp = primesInRange;
 
-        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 0; i < prime.length; i++){
+            if (prime[i]) {
+                primesInRange++;
+            }
+        }
+
+        System.out.println("There are " + primesInRange + " number of primes up to " + n + "\n");
         int count = 0;
-        if (primesInRangeTemp > 5) {
+        if (primesInRange > 5) {
             for (int i = 2; i <= n; i++) {
-                if (prime[i]) {
+                if (prime[i] && count < 3) {
                     System.out.print(i + ", ");
-                    count++;
                 }
-                if (count == 3) {
-                    break;
+                else if (prime[i] && (count == primesInRange - 2)){
+                    System.out.print(i + ", ");
                 }
-            }
+                else if (prime[i] && (count == primesInRange - 1)) {
+                    System.out.println(i + " ");
+                }
 
-            //todo burası yanlış tersten yazıyor mantıklı bir çözüm bul
-            for (int i = n; i >= 2; i--) {
-                if (prime[i] && count == 4) {
-                    System.out.println(i);
-                    break;
-                }
-                if (prime[i]) {
-                    System.out.print(i + ", ");
+                if (prime[i]){
                     count++;
                 }
             }
-
         } else {
             for (int i = 2; i <= n; i++) {
                 if (primesInRange > 0) {
@@ -136,26 +136,38 @@ public class secondary {
     }
 
     public static void sundaram(int n) {
-        boolean[] marked = new boolean[n/2 + 1];
+        int nHalf = n/2;
+        boolean[] marked = new boolean[nHalf + 1];
         long startTime = System.nanoTime();
 
-        for (int i = 1; i <= n/2; i++) {
-            for (int j = i; (i + j + (2 * i * j)) <= (n/2); j++){
-                marked[i+j+(i*j*2)] = true;
+        for (int i = 1; i <= nHalf; i++) {
+            for (int j = i; ; j++){
+                long index = (long)i + (long)j + (2L * i * j);
                 
+                if (index > nHalf) break;
+
+                marked[(int)index] = true;
             }
         }
-        Long endTime = System.nanoTime();
-
-        System.out.print("2");
+        
+        List<Integer> primes = new ArrayList<>();
+        primes.add(0, 2);
+        // todo tüm asal sayıları yazıyor, sadece ilk 3 ve sondan 2 asal sayıyı yazması lazım
         for (int i = 1; i <= n/2; i++) {
             if (!marked[i]) {
                 if (2* i + 1 == 2) {
                     continue;
                 } else if (2 * i + 1 <= n)
-                    System.out.print(", " +(2 * i + 1));
+                    primes.add((2 * i + 1));
             }
         }
+
+        Long endTime = System.nanoTime();
+        System.out.print(primes.get(0) + ", ");
+        System.out.print(primes.get(1) + ", ");
+        System.out.print(primes.get(2) + ", ");
+        System.out.print(primes.get(primes.size()-2) + ", ");
+        System.out.println(primes.get(primes.size()-1));
         System.out.println();
         Long totalTime = endTime - startTime;
         System.out.println("Time taken to compute primes up to " + n + ": " + totalTime + " nanoseconds");
@@ -164,7 +176,97 @@ public class secondary {
     }
 
     public static void atkin(int n) {
+        boolean[] marked = new boolean[n + 1];
+        marked[2] = true;
+        marked[3] = true;
+        Long startTime = System.nanoTime();
+        int primesInRange = 0;
 
+        for (int x = 1; x*x <= n; x++){
+            for (int y = 1; y*y <=n; y++) {
+                //step 1
+                long n1 = (4L*x*x) + (y*y);
+                if (n1 <= n) {
+                    if (n1 % 12 == 1 || n1 % 12 == 5) {
+                        if(marked[(int)n1]) {
+                            marked[(int)n1] = false;
+                        } else {
+                            marked[(int)n1] = true;
+                        }
+                    }
+                }
+
+                //step 2
+                long n2 = (3L*x*x) + (y*y);
+                if(n2 <= n) {
+                    if (n2 % 12 == 7) {
+                        if(marked[(int)n2]) {
+                            marked[(int)n2] = false;
+                        } else {
+                            marked[(int)n2] = true;
+                        }
+                    }
+                }
+
+                //step 3
+                if (x > y) {
+                    long n3 = (3L*x*x) - (y*y);
+                    if (n3 <= n) {
+                        if (n3 % 12 == 11) {
+                            if(marked[(int)n3]) {
+                                marked[(int)n3] = false;
+                            } else {
+                                marked[(int)n3] = true;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        for (int r = 5; (long)r * r <= n; r++) { 
+            if (marked[r]) {
+                long rSquare = (long)r * r;
+
+                for (long i = rSquare; i <= n; i += rSquare) {
+                
+                    marked[(int)i] = false;
+                }
+            }
+        }
+
+        Long endTime = System.nanoTime();
+
+
+        for (int i = 0; i < marked.length; i++) {
+            if (marked[i]){
+                primesInRange++;
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (marked[i] && count < 3) {
+                System.out.print(i + ", ");
+            }
+            else if (marked[i] && (count == primesInRange - 2)){
+                System.out.print(i + ", ");
+            }
+            else if (marked[i] && (count == primesInRange - 1)) {
+                System.out.println(i + " ");
+            }
+
+            if (marked[i]){
+                count++;
+            }
+        }
+
+
+        System.out.println();
+        Long totalTime = endTime - startTime;
+        System.out.println("Time taken to compute primes up to " + n + ": " + totalTime + " nanoseconds");
+        System.out.println("-------------------------------");
     }
 }
 
